d584bcdef036c061194ca9d5b0891e394cb65d10
ChrisK1504 <krismehilli@yahoo.com>
2024-09-11 19:24:56 +0200

    Created a simple Navbar component; Included it in App.jsx;

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import {appleImg} from "../utils"
+const Navbar = () => {
+  return (
+    <header>
+        <nav>
+            <img src={appleImg} alt="Apple" width={14}
+            height={18} />
+
+            <div>
+                {['Phones','MacBooks','Tablets'].map(
+                    (nav, i) => (
+                        <div>
+                            {nav}
+                        </div>
+                    )
+                )}
+            </div>
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
