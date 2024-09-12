c7a79ad12f3c93cc7ada0978dc62bb26740ff852
ChrisK1504 <krismehilli@yahoo.com>
2024-09-12 18:45:17 +0200

    Implemented a Canvas tag, imported from three/fiber, where the model will be showcased; Implemented 4 list elements as colored circles which will be able to be clicked on to change the mesh color of the model;

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -5,6 +5,9 @@ import { useRef, useState } from "react"
 import { yellowImg } from "../utils"
 
 import * as THREE from 'THREE'
+import { Canvas } from "@react-three/fiber"
+import { View } from "@react-three/drei"
+import { models } from "../constants"
 
 const Model = () => {
   const [size, setSize] = useState('small');
@@ -58,6 +61,32 @@ const Model = () => {
               item={model}
               size={size}
               />
+            <Canvas className="w-full h-full"
+            style={{
+              position: 'fixed',
+              top: 0,
+              bottom: 0,
+              left: 0,
+              right: 0,
+              overflow: 'hidden'
+            }}
+            eventSource={document.getElementById('root')}
+            >
+              <View.Port />
+            </Canvas>
+          </div>
+
+          <div className="mx-auto w-full">
+            <p className="text-sm font-light text-center mb-5">{model.title}</p>
+            <div className="flex-center">
+              <ul className="color-container">
+                {models.map((item, i) => (
+                  <li key={i} className="w-6 h-6 rounded-full mx-2"
+                  style={{backgroundColor: item.color[0]}}
+                  onClick={() => setModel(item)}/>
+                ))}
+              </ul>
+            </div>
           </div>
         </div>
       </div>
