0e74cde38e000049c75a89779560f6ea6ac8826a
ChrisK1504 <krismehilli@yahoo.com>
2024-09-13 12:55:51 +0200

    Added a visible border to the ModelView;

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,6 +1,17 @@
-const ModelView = () => {
+import { View } from "@react-three/drei"
+
+const ModelView = ({index, groupRef, gsapType,
+  ControlRef, setRotationSize, size, item
+}) => {
   return (
-    <div>ModelView</div>
+    <View
+      index={index}
+      id={gsapType}
+      className={`border-2 border-red-500 w-full h-full 
+        ${index===2} ? 'right-[-100%] : ''`}
+    >
+
+    </View>
   )
 }
 
