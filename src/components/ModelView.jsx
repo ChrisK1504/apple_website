4083609cb1f06b5cc19696cb0bcee3a2601aa8d2
ChrisK1504 <krismehilli@yahoo.com>
2024-09-13 14:11:37 +0200

    Utilized GSAP timeline functionality to implement the conversion of the views from one iphone model from the other; Fixed a Bug where the models would appear on top of each other;

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -4,16 +4,13 @@ import Iphone from './Iphone'
 import { Suspense } from "react"
 import * as THREE from 'THREE'
 const ModelView = ({index, groupRef, gsapType,
-  controlRef, setRotationSize, size, item
+  controlRef, setRotationState, size, item
 }) => {
   return (
     <View
       index={index}
       id={gsapType}
-      className={`border-2 border-red-500 w-full h-full 
-        ${index===2} ? 'right-[-100%] : ''`}
-    >
-      {/* Ambient Light */}
+      className={`w-full h-full absolute ${index === 2 ? 'right-[-100%]' : ''}`}>
       <ambientLight intensity={0.3} />
 
       <PerspectiveCamera makeDefault position={[0,0,4]} />
