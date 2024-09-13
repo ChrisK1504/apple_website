4bd02ef57eede3c5214fd19ee9038ddf2b225f74
ChrisK1504 <krismehilli@yahoo.com>
2024-09-13 13:49:56 +0200

    Modified the Orbital Controls of the model; Also added scale to the model;

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,9 +1,10 @@
-import { Html, PerspectiveCamera, View } from "@react-three/drei"
+import { Html, OrbitControls, PerspectiveCamera, View } from "@react-three/drei"
 import Lights from './Lights'
 import Iphone from './Iphone'
 import { Suspense } from "react"
+import * as THREE from 'THREE'
 const ModelView = ({index, groupRef, gsapType,
-  ControlRef, setRotationSize, size, item
+  controlRef, setRotationSize, size, item
 }) => {
   return (
     <View
@@ -18,9 +19,26 @@ const ModelView = ({index, groupRef, gsapType,
       <PerspectiveCamera makeDefault position={[0,0,4]} />
 
       <Lights />
+      <OrbitControls 
+        makeDefault
+        ref={controlRef}
+        enableZoom={false}
+        enablePan={false}
+        rotateSpeed={0.4}
+        target={new THREE.Vector3}
+        onEnd={() => setRotationState(controlRef.current.getAzimuthalAngle())}
+      />
+
+      <group ref={groupRef} name={`${index === 1} ? 'small' : 'large`} position={[0, 0 ,0]}>
       <Suspense fallback={<Html><div>Loading</div></Html>}>
-        <Iphone />
+        <Iphone 
+          scale={index === 1 ? [15,15,15] : [17,17,17]}
+          item={item}
+          size={size}
+        />
       </Suspense>
+
+      </group>
     </View>
   )
 }
